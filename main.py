66b79724b643f436977958a24365046f3f2968ca
WizziWizBiz <164041753+WizziWizBiz@users.noreply.github.com>
2025-01-31 13:13:42 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,3 @@
+"""This program prompts inputs for number of cards that the user wants to play on turns 1-4, and the number of lands in the deck.
+  The program then calculates the odds of playing a land and a card on turns 1-5, with turn 5 being the commander.
+  It will then output this result, along with the number of remaining slots in the deck for other cards (e.g. removal, draw, etc)."""
